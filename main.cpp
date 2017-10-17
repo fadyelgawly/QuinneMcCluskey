a90bb3039b68d433cae041ff168d67fe30d4acb4
Fady Hanna <Fady Hanna>
2017-10-17 15:33:54 +0200

    Mark Jacoub's commit, decimal_binary function was adjusted to decimalToBinaryString, braces were added to if elses to elimanate warnings

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-//
-//  main.cpp
-//  QM
-//
-//  Created by Fady Hanna on 10/17/17.
-//  Copyright © 2017 Fady Hanna. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+struct Terms
+{
+    string binary; // 1010
+    int decimal;
+    bool min = false;
+    int number_of_1;
+};
 
 std::string decimalToBinaryString(int num, int numberOfVariables){
     std::string str ;
@@ -25,8 +30,96 @@ std::string decimalToBinaryString(int num, int numberOfVariables){
 }
 
 
+
+
+
+
+int Input(int variables, vector<Terms> minterm)
+{
+    int totalTerms;
+    int total = pow(2, variables);
+    Terms process;
+    /*------------------------------Minterms Entery-------------------------------*/
+    
+    cout << "Choose the minterms between 0  and " << total - 1 << endl << " When done press -1" << endl;
+    int i = 0;
+    while (i != -1)
+    {
+        cin >> i;
+        if (i != -1){
+            if (i >= 0 && i < total) {
+                {
+                    process.min = true;
+                    process.decimal = i;
+                    process.binary = decimalToBinaryString(i, variables);
+                    minterm.push_back(process);
+                }
+            }
+        }
+            else {
+                cout << "out of boundries try again" << endl;
+                
+            }
+    }
+    /*------------------------------Dont Care Entery-------------------------------*/
+    cout << "Choose the DontCare between 0  and " << total - 1 << endl << " When done press -1" << endl;
+    
+    int j = 0;
+    while (j != -1)
+    {
+        cin >> j;
+        if (j != -1){
+            if (j>= 0 && j < total) {
+                {
+                    process.min = false;
+                    process.decimal = j;
+                    process.binary = decimalToBinaryString(j, variables);
+                    minterm.push_back(process);
+                    
+                }
+            }}
+        else{
+            cout << "out of boundries try again" << endl;
+            
+        }
+    }
+    return totalTerms = minterm.size();
+    
+}
+
+void Print(int total, vector<Terms> minterm)
+{
+    
+    for (int i = 0; i < total; i++)
+    {
+        if (minterm[i].min == true)
+            cout << "m" << minterm[i].decimal << " = " << minterm[i].binary << endl;
+        else
+            cout << "D" << minterm[i].decimal << " = " << minterm[i].binary << endl;
+    }
+}
+
+
+
+
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    
+    int variables,totalTerms;
+    vector<Terms> minterm;
+    cout << "Enter Number of variables" << endl;
+    cin >> variables;
+    totalTerms = Input(variables, minterm); //User input
+    //minterm.resize(totalTerms);
+    Print(totalTerms,minterm);    //Print all the Minterms and dont cares
+    
+    system("pause");
+    
+    
     return 0;
 }
+
+
+
